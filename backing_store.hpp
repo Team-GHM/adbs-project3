c1f53f0d114e3826c2901aed2c77237141369273
Gabriel Kerr <mgabrielkerr@gmail.com>
2023-10-28 10:32:30 -0600

    Formatting changes and add epsilon variable

--- backing_store.hpp
+++ backing_store.hpp
@@ -8,25 +8,27 @@
 #include <cstddef>
 #include <iostream>
 
-class backing_store {
+class backing_store
+{
 public:
-  virtual void   allocate(uint64_t obj_id, uint64_t version) = 0;
+  virtual void allocate(uint64_t obj_id, uint64_t version) = 0;
   virtual void deallocate(uint64_t obj_id, uint64_t version) = 0;
-  virtual std::iostream * get(uint64_t obj_id, uint64_t version) = 0;
-  virtual void            put(std::iostream *ios) = 0;
+  virtual std::iostream *get(uint64_t obj_id, uint64_t version) = 0;
+  virtual void put(std::iostream *ios) = 0;
 };
 
-class one_file_per_object_backing_store: public backing_store {
+class one_file_per_object_backing_store : public backing_store
+{
 public:
   one_file_per_object_backing_store(std::string rt);
-  void	  allocate(uint64_t obj_id, uint64_t version);
-  void		  deallocate(uint64_t obj_id, uint64_t version);
-  std::iostream * get(uint64_t obj_id, uint64_t version);
-  void            put(std::iostream *ios);
+  void allocate(uint64_t obj_id, uint64_t version);
+  void deallocate(uint64_t obj_id, uint64_t version);
+  std::iostream *get(uint64_t obj_id, uint64_t version);
+  void put(std::iostream *ios);
   std::string get_filename(uint64_t obj_id, uint64_t version);
-  
+
 private:
-  std::string	root;
+  std::string root;
 };
 
 #endif // BACKING_STORE_HPP
