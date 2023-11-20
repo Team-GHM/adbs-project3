a8944f1da29106b8f6feaea837914ca8bdcead0d
Harnoor Bagga <harnoorbagga98@gmail.com>
2023-11-20 02:15:11 -0700

    clean up on python files

--- graph_queries.py
+++ graph_queries.py
@@ -1,32 +1,3 @@
-"""import matplotlib.pyplot as plt
-
-# Load the data from the output file
-with open('read_ops_times_new.txt', 'r') as file:
-    lines = file.readlines()
-
-nops = []
-ops_times = []
-
-for line in lines:
-    values = line.split()
-    nops.append(int(values[0]))
-    ops_times.append(float(values[1]))
-
-# Create the time series graph
-plt.figure(figsize=(10,8))
-plt.plot(nops, ops_times, label='Average time taken by operations')
-plt.xlabel('nops')
-plt.ylabel('Average time taken for 100 operations (μs)')
-plt.title('Time taken to perform nops')
-plt.legend()
-plt.grid()
-
-#y_tick_positions = range(int(min(throughputs)), int(max(throughputs) + 1), 10000)  # Custom interval of 10000
-#plt.yticks(y_tick_positions, [str(val) for val in y_tick_positions])
-
-plt.savefig('/home/u1418793/adbs-project3/queries.png') 
-plt.show()"""
-
 import matplotlib.pyplot as plt
 
 # Function to load data from a file
